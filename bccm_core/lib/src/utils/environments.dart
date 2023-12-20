b29883de1abfd14205e13b042160b15b84dc59bc
Andreas Gangsø <andreasgangso@gmail.com>
2023-12-20 11:10:09 +0100

    feat: environments

---
+++ bccm_core/lib/src/utils/environments.dart
@@ -0,0 +1,13 @@
+const apiEnvUrls = <String, String>{
+  EnvironmentOverride.dev: 'https://api.dev.brunstad.tv/query',
+  EnvironmentOverride.sta: 'https://api.sta.brunstad.tv/query',
+  EnvironmentOverride.prod: 'https://api.brunstad.tv/query',
+};
+
+class EnvironmentOverride {
+  EnvironmentOverride._();
+  static const dev = 'dev';
+  static const sta = 'sta';
+  static const prod = 'prod';
+  static const none = 'none';
+}
