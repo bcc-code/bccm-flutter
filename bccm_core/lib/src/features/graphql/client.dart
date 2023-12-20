968a37e9aeac702fdbd6bbef20fc89d780deb28f
Andreas Gangsø <andreasgangso@gmail.com>
2023-12-20 13:34:03 +0100

    typed graphql headers

--- bccm_core/lib/src/features/graphql/client.dart
+++ bccm_core/lib/src/features/graphql/client.dart
@@ -5,3 +5,29 @@ import 'package:graphql/client.dart';
 final bccmGraphQLProvider = Provider<GraphQLClient>((ref) {
   throw UnimplementedError("bccmGraphQLProvider is not overriden. Please override it in main().");
 });
+
+class BccmGraphqlHeaders {
+  BccmGraphqlHeaders({
+    required this.acceptLanguage,
+    required this.application,
+    required this.applicationVersion,
+    required this.featureFlags,
+    required this.extraUsergroups,
+  });
+
+  final List<String> acceptLanguage;
+  final String application;
+  final String applicationVersion;
+  final String featureFlags;
+  final List<String> extraUsergroups;
+
+  Map<String, String> toMap() {
+    return {
+      'Accept-Language': acceptLanguage.join(','),
+      'X-Application': application,
+      'X-Application-Version': applicationVersion,
+      'X-Feature-Flags': featureFlags,
+      if (extraUsergroups.isNotEmpty) 'x-explicit-roles': extraUsergroups.join(','),
+    };
+  }
+}
