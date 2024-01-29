fe66a5453e9dd2359684c2e72aeaa55b37a6e29f
Andreas Gangsø <andreasgangso@gmail.com>
2024-01-29 13:31:47 +0100

    remove local notifications for androidtv

--- bccm_core/lib/src/features/notifications/notification_service.dart
+++ bccm_core/lib/src/features/notifications/notification_service.dart
@@ -1,17 +1,14 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 import 'package:auto_route/auto_route.dart';
 import 'package:bccm_core/bccm_core.dart';
-import 'package:bccm_core/src/features/notifications/local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Each app should override this provider with e.g. [FcmNotificationService].
 final notificationServiceProvider = Provider<NotificationService>((ref) {
-  debugPrint('WARNING: notificationServiceProvider is not overriden. Using DisabledNotificationService.');
+  debugPrint(
+      'WARNING: notificationServiceProvider is not overriden. Using DisabledNotificationService.');
   return DisabledNotificationService();
 });
 
@@ -34,25 +31,25 @@ class DisabledNotificationService implements NotificationService {
 }
 
 class FcmNotificationService implements NotificationService {
-  final LocalNotificationService localNotificationService;
   String? fcmToken;
   late StreamSubscription<AppReadyEvent> _appReadySubscription;
   StreamSubscription<RemoteMessage>? _onMessageSubscription;
   StreamSubscription<RemoteMessage>? _onMessageOpenedApp;
   StreamSubscription<String>? _tokenSubscription;
   void Function(String token) onTokenChanged;
-  final void Function(RemoteMessage? message)? onAppOpenWhenNotificationReceived;
+  final void Function(RemoteMessage? message)?
+      onAppOpenWhenNotificationReceived;
   final void Function(RemoteMessage? message)? onShowInAppRequested;
   final void Function(RemoteMessage? message)? onCacheClearRequested;
 
   FcmNotificationService({
-    required this.localNotificationService,
     required this.onTokenChanged,
     required this.onAppOpenWhenNotificationReceived,
     required this.onShowInAppRequested,
     required this.onCacheClearRequested,
   }) {
-    _appReadySubscription = globalEventBus.on<AppReadyEvent>().listen(_onAppReady);
+    _appReadySubscription =
+        globalEventBus.on<AppReadyEvent>().listen(_onAppReady);
   }
 
   @override
@@ -63,19 +60,7 @@ class FcmNotificationService implements NotificationService {
     _tokenSubscription?.cancel();
   }
 
-  void _setupLocalNotifications() {
-    localNotificationService.stream.listen(_onLocalNotificationOpened);
-  }
-
-  void _onLocalNotificationOpened(NotificationResponse response) {
-    final payload = response.payload;
-    if (payload == null) return;
-    final data = jsonDecode(payload) as Map<String, dynamic>;
-    if (data['firebase_foreground'] != true) return;
-    final action = data['action'] as String?;
-    if (action == null) return;
-    _handleAction(data: data, openedFromBackground: true);
-  }
+  void _setupLocalNotifications() {}
 
   void _onTokenChanged(String token) {
     fcmToken = token;
@@ -94,7 +79,8 @@ class FcmNotificationService implements NotificationService {
   Future<void> requestPermissionAndSetup() async {
     var result = await FirebaseMessaging.instance.requestPermission();
     debugPrint('NotificationStatus: ${result.authorizationStatus}');
-    FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(alert: true, badge: false, sound: false);
+    FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+        alert: true, badge: false, sound: false);
     _setupLocalNotifications();
 
     final token = await FirebaseMessaging.instance.getToken();
@@ -107,10 +93,11 @@ class FcmNotificationService implements NotificationService {
   }
 
   void _setupTokenListeners() {
-    _tokenSubscription ??= FirebaseMessaging.instance.onTokenRefresh.listen(_onTokenChanged)
-      ..onError((err) {
-        debugPrint('error onTokenRefresh');
-      });
+    _tokenSubscription ??=
+        FirebaseMessaging.instance.onTokenRefresh.listen(_onTokenChanged)
+          ..onError((err) {
+            debugPrint('error onTokenRefresh');
+          });
   }
 
   void _onAppReady(AppReadyEvent _) async {
@@ -122,65 +109,37 @@ class FcmNotificationService implements NotificationService {
 
     FirebaseMessaging.instance.setDeliveryMetricsExportToBigQuery(true);
 
-    _onMessageSubscription ??= FirebaseMessaging.onMessage.listen(_handleMessage);
-    _onMessageOpenedApp ??= FirebaseMessaging.onMessageOpenedApp.listen((message) {
+    _onMessageSubscription ??=
+        FirebaseMessaging.onMessage.listen(_handleMessage);
+    _onMessageOpenedApp ??=
+        FirebaseMessaging.onMessageOpenedApp.listen((message) {
       debugPrint('_onMessageOpenedApp');
       _handleMessage(message, openedFromBackground: true);
     });
     debugPrint('Notification listeners set up.');
   }
 
-  void _handleMessage(RemoteMessage message, {bool openedFromBackground = false}) {
+  void _handleMessage(RemoteMessage message,
+      {bool openedFromBackground = false}) {
     if (!openedFromBackground) {
       debugPrint('Got a message whilst in the foreground!');
     }
-    debugPrint('Message data: ${message.data}, notification: ${message.notification?.title}');
+    debugPrint(
+        'Message data: ${message.data}, notification: ${message.notification?.title}');
 
     final notification = message.notification;
     if (!openedFromBackground && notification != null) {
       onAppOpenWhenNotificationReceived?.call(message);
-      if (Platform.isAndroid) {
-        _showLocalNotification(message);
-      }
     }
-    if (openedFromBackground && notification != null && message.data['show_in_app'] == true) {
+    if (openedFromBackground &&
+        notification != null &&
+        message.data['show_in_app'] == true) {
       onShowInAppRequested?.call(message);
     }
     _handleAction(
       data: message.data,
       openedFromBackground: openedFromBackground,
       message: message,
-    );
-  }
-
-  _showLocalNotification(RemoteMessage message) {
-    final context = globalNavigatorKey.currentContext;
-    if (context == null) {
-      debugPrint('No context to open notification');
-      return;
-    }
-    final notification = message.notification;
-    if (notification == null) {
-      debugPrint('No notification to open');
-      return;
-    }
-    final title = notification.title;
-    final body = notification.body;
-    debugPrint('Showing notification: $title, $body');
-    FlutterLocalNotificationsPlugin().show(
-      notification.hashCode,
-      title,
-      body,
-      const NotificationDetails(
-        android: AndroidNotificationDetails(
-          'high_importance_channel',
-          'High Importance Notifications',
-        ),
-      ),
-      payload: jsonEncode({
-        'firebase_foreground': true,
-        ...message.data,
-      }),
     );
   }
 
@@ -191,14 +150,16 @@ class FcmNotificationService implements NotificationService {
   }) {
     final context = globalNavigatorKey.currentContext;
     if (context?.mounted != true) {
-      debugPrint('Navigator was not mounted while handling an FCM message. Aborting.');
+      debugPrint(
+          'Navigator was not mounted while handling an FCM message. Aborting.');
       return;
     }
     if (openedFromBackground && data['action'] == 'deep_link') {
       if (data['deep_link'] is String) {
         String path = data['deep_link'];
         debugPrint('navigating to deep_link from notification: $path');
-        debugPrint('router in notification handler: ${context!.router.currentPath} ${context.router.currentSegments}');
+        debugPrint(
+            'router in notification handler: ${context!.router.currentPath} ${context.router.currentSegments}');
         context.router.navigateNamedFromRoot(path);
       }
     }
