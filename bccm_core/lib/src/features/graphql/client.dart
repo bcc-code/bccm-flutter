0db7ccbce4d7de8bc5021bced4bf1fb0e469690d
Andreas Gangsø <andreasgangso@gmail.com>
2024-07-12 15:06:10 +0200

    feat: send os info to backend

--- bccm_core/lib/src/features/graphql/client.dart
+++ bccm_core/lib/src/features/graphql/client.dart
@@ -1,3 +1,4 @@
+import 'package:bccm_core/bccm_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:graphql/client.dart';
 
@@ -13,6 +14,9 @@ class BccmGraphqlHeaders {
     required this.applicationVersion,
     required this.featureFlags,
     required this.extraUsergroups,
+    required this.isTablet,
+    required this.os,
+    required this.osVersion,
   });
 
   final List<String> acceptLanguage;
@@ -20,12 +24,18 @@ class BccmGraphqlHeaders {
   final String applicationVersion;
   final List<String>? featureFlags;
   final List<String> extraUsergroups;
+  final bool isTablet;
+  final String os;
+  final String osVersion;
 
   Map<String, String> toMap() {
     return {
       'Accept-Language': acceptLanguage.join(','),
       'X-Application': application,
       'X-Application-Version': applicationVersion,
+      'X-Is-Tablet': isTablet ? 'true' : 'false',
+      'X-OS': os,
+      'X-OS-Version': osVersion,
       if (featureFlags?.isNotEmpty == true) 'X-Feature-Flags': featureFlags!.join(','),
       if (extraUsergroups.isNotEmpty) 'x-explicit-roles': extraUsergroups.join(','),
     };
