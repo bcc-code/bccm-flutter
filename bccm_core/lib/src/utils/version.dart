b6db3010221bf535e63c878047bf080f5a0dc319
Sigve Hansen <sighan12@gmail.com>
2025-06-24 10:20:29 +0200

    fix: fallback version number to 0

--- bccm_core/lib/src/utils/version.dart
+++ bccm_core/lib/src/utils/version.dart
@@ -1,6 +1,8 @@
 import 'package:package_info_plus/package_info_plus.dart';
 
 int getExtendedVersionNumber(String version) {
+  if (version.isEmpty) return 0;
+
   List versionCells = version.split('.');
   versionCells = versionCells.map((i) => int.parse(i)).toList();
   return versionCells[0] * 100000 + versionCells[1] * 1000 + versionCells[2];
